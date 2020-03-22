50985df143fdfde2a0753d5e0b994bc5225a7f67
tihon-end <55427640+tihon-end@users.noreply.github.com>
2020-03-22 21:05:06 +0300

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,24 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+   if(options.separator === undefined) options.separator = '+'
+  if(options.additionSeparator === undefined) options.additionSeparator = ''
+  if(options.repeatTimes === undefined) options.repeatTimes = 1
+  if(options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1
+  if(options.addition === undefined) options.additionRepeatTimes = 0
+
+  let res = ''
+
+  for(let i = 0; i < options.repeatTimes; ++i) {
+      res += str
+
+      for(let j = 0; j < options.additionRepeatTimes; ++j) {
+          res += options.addition
+
+          if(j < options.additionRepeatTimes - 1) res += options.additionSeparator
+      }
+
+      if(i < options.repeatTimes - 1) res += options.separator
+  }
+
+  return res
+}
   
