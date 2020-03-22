d41f57581c9db377236bf34eae2675a3d4a470c2
tihon-end <55427640+tihon-end@users.noreply.github.com>
2020-03-22 21:29:58 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,43 @@
 const chainMaker = {
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+   this.chain.push(value)
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+   if(typeof(position) !== 'number') {
+      this.chain = []
+      throw new Error()
+    }
+    position--
+    if(position < 0 || position >= this.getLength()) {
+      this.chain = []
+      throw new Error()
+    }
+
+    this.chain.splice(position, 1)
+    return this
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
-};
+    let res = ''
 
-module.exports = chainMaker;
+    for(let i = 0; i < this.getLength(); ++i) {
+      res += `( ${this.chain[i]} )`
+
+      if(i < this.getLength() - 1) {
+        res += '~~'
+      }
+    }
+
+    this.chain = [];
+    return res
+  }
+}
+
+module.exports = chainMaker
