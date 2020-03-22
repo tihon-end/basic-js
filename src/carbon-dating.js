46f3a51b53596774ebe3177893bed1cad9448060
tihon-end <55427640+tihon-end@users.noreply.github.com>
2020-03-22 20:56:37 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,24 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity ) {
+if(typeof sampleActivity === 'string') {
+    if(!/^[0-9]*[\.]{0,1}[0-9]*[\.]{0,1}[0-9]*$/.test(sampleActivity)) {
+      return false;
+    }
+
+
+
+    const activity = parseFloat(sampleActivity.match(/^[0-9]*[\.]{0,1}[0-9]*/));
+
+    if(activity <= 0 || activity >= 15) {
+      return false;
+    }
+
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    const res = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+    return res;
+  }
+  return false;
+}
+
