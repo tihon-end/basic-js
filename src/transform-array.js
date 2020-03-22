978c244b78ba857e0b564c6aca14fb27d1afb29a
tihon-end <55427640+tihon-end@users.noreply.github.com>
2020-03-22 21:19:18 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+ if (!Array.isArray(arr)) throw new Error()
+  let length = arr.length
+  let result = []
+
+  for (let i = 0; i < length; i++) {
+    switch (arr[i]) {
+      case '--discard-prev':
+        if (result.length) result.pop()
+        break
+      case '--double-prev':
+        if (i) result.push(arr[i - 1])
+        break
+      case '--double-next':
+        if (i < length - 1) result.push(arr[i + 1])
+        break;
+      case '--discard-next':
+        i++;
+        break
+      default: 
+        result.push(arr[i])
+    }
+  }
+
+  return result
+}
